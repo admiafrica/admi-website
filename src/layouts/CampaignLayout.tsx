66e3fbb17e2b367faf0dae5452520edab75eb8c1
Wasim Al-Haddad <wasim.al-haddad@craydel.com>
2024-09-17 16:46:34 +0300

    Added favicon

--- src/layouts/CampaignLayout.tsx
+++ src/layouts/CampaignLayout.tsx
@@ -1,5 +1,6 @@
 import styles from '@/assets/css/main.module.css';
 import { CampaignHeader } from '../components/campaign';
+import Head from 'next/head';
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -8,6 +9,11 @@ type LayoutProps = {
 export function CampaignLayout({ children }: LayoutProps) {
   return (
     <>
+      <Head>
+        <title>Africa Digital Media Institute (ADMI)</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <CampaignHeader />
       <main className={styles['campaign-main']}>
         {children}
