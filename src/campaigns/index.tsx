85de8d4295985232262bd9c39b3c88dd293b5717
Esikanda Mbeche <stephen.mbeche@craydel.com>
2024-09-18 10:29:01 +0300

    added image domain url on next.config

--- src/campaigns/index.tsx
+++ src/campaigns/index.tsx
@@ -10,23 +10,29 @@ import {
 } from '@/components/campaign';
 import courseImage from '@/assets/images/course-banner.webp';
 import styles from '@/assets/css/main.module.css';
-
+import { useRouter } from 'next/router';
 export function CampaignsPage() {
   const [bannerSrc, setBannerSrc] = useState(courseImage);
   const [bannerAlt, setBannerAlt] = useState('Course image');
   const [isLeadFormVisible, setIsLeadFormVisible] = useState(false);
+  const router = useRouter();
+  console.log('country' +  JSON.stringify(router.query.country))
+  console.log('category' +  JSON.stringify(router.query.category))
+  console.log('campaign' +  JSON.stringify(router.query.campaign))
 
   useEffect(() => {
     // Simulate fetching data from an API
     const fetchImageData = async () => {
       try {
         // Fetch image data from API (replace with actual API)
-        const response = await fetch('https://example.com/api/image');
+
+        const response = await fetch('https://randomuser.me/api/?results=5');
         const data = await response.json();
 
+
         // Set image src and alt from the API response
-        setBannerSrc(data.course_image);
-        setBannerAlt(data.course_name);
+        setBannerSrc(data.results[0].picture.thumbnail);
+        setBannerAlt(data.results[0].name.first);
       } catch (error) {
         console.error('Error fetching image data:', error);
       }
