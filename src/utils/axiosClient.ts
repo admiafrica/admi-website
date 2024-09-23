ded3ee0e77b8cf040931ac63eae9856583937da3
Esikanda Mbeche <stephen.mbeche@craydel.com>
2024-09-23 19:54:55 +0300

    Configure API client to include authorization token from .env

--- src/utils/axiosClient.ts
+++ src/utils/axiosClient.ts
@@ -1,14 +1,23 @@
+// noinspection JSAnnotator
+
 import axios from 'axios';
+const token = process.env.NEXT_PUBLIC_ACCESS_TOKEN; // Access token from .env
+
+ // Ensure this is a string
 
 const apiClient = axios.create({
     baseURL: process.env.NEXT_PUBLIC_ADMI_SERVICES_API,
-    timeout: 10000, // Optional: Set a timeout for all requests
+    headers: {
+        'Content-Type': 'application/json', // Default content type for all requests
+        'Authorization': `Bearer ${token}` // Add the token to the headers
+    },
+    timeout: 10000 // Optional: Set a timeout for all requests
 });
 
 apiClient.interceptors.response.use(
     response => response,
     error => {
-        // Add custom error handling logic, e.g., refresh tokens incase needed
+        // Add custom error handling logic, e.g., refresh tokens if needed
         return Promise.reject(error);
     }
 );
