598a408a911e61cf990ff94beb62f7c8254ec621
Esikanda Mbeche <stephen.mbeche@craydel.com>
2024-10-18 13:58:41 +0300

    move the GM code to .env

--- src/campaigns/craydel/thanks.tsx
+++ src/campaigns/craydel/thanks.tsx
@@ -7,55 +7,54 @@ import React from 'react';
 import Head from "next/head";
 
 export function ThankYouPage() {
-  return (
-      <>
-        <Head>
-          {process.env.BUILD_ENV === 'production' && (
-              <script
-                  dangerouslySetInnerHTML={{
-                    __html: `
+    const gtmId = process.env.NEXT_PUBLIC_GTM_ID; // Fetch the GTM ID from .env
+    return (
+        <>
+            <Head>
+                {process.env.BUILD_ENV === 'production' && gtmId && (
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
                 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-NQLRWC7Q');
+                })(window,document,'script','dataLayer','${gtmId}');
               `
-                  }}
-              />
-          )}
-        </Head>
+                        }}
+                    />
+                )}
+            </Head>
 
-        {process.env.BUILD_ENV === 'production' && (
-            <noscript>
-              <iframe
-                  src="https://www.googletagmanager.com/ns.html?id=GTM-NQLRWC7Q"
-                  height="0"
-                  width="0"
-                  style={{ display: 'none', visibility: 'hidden' }}>
-              </iframe>
-            </noscript>
-        )}
-        <EmptyLayout client="craydel">
-          <section className={`${styles['section-wrapper']} ${styles['thank-you-section']}`}>
-            <div className={`${styles['wrapper']}`}>
-              <div className={`${styles['thank-you-card']}`}>
-                <div className={`${styles['thank-you-card--icon']}`}>
-                  <Image fetchPriority="high" src={successIcon.src} alt="Success" width={100} height={100} />
-                </div>
-                <h1 className={`${styles['section-title']} ${styles['section-title--small']}`}>Thank you!</h1>
-                <p>Thank you for your enquiry. Our team will reach out with more information about our courses.</p>
-                <a href="https://admi.africa/" className={`${styles['btn']} ${styles['btn-primary']}`}>Back to our
-                  website</a>
+            {process.env.BUILD_ENV === 'production' && gtmId && (
+                <noscript>
+                    <iframe
+                        src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+                        height="0"
+                        width="0"
+                        style={{ display: 'none', visibility: 'hidden' }}
+                    ></iframe>
+                </noscript>
+            )}
 
-                <a href="https://admi.africa" className={`${styles['site-logo']}`}>
-                  <Image src={logo.src} alt={'ADMI'} width={90} height={90}></Image>
-                </a>
-              </div>
-            </div>
-          </section>
-        </EmptyLayout>
-
-      </>
+            <EmptyLayout client="craydel">
+                <section className={`${styles['section-wrapper']} ${styles['thank-you-section']}`}>
+                    <div className={`${styles['wrapper']}`}>
+                        <div className={`${styles['thank-you-card']}`}>
+                            <div className={`${styles['thank-you-card--icon']}`}>
+                                <Image fetchPriority="high" src={successIcon.src} alt="Success" width={100} height={100} />
+                            </div>
+                            <h1 className={`${styles['section-title']} ${styles['section-title--small']}`}>Thank you!</h1>
+                            <p>Thank you for your enquiry. Our team will reach out with more information about our courses.</p>
+                            <a href="https://admi.africa/" className={`${styles['btn']} ${styles['btn-primary']}`}>Back to our website</a>
+                            <a href="https://admi.africa" className={`${styles['site-logo']}`}>
+                                <Image src={logo.src} alt={'ADMI'} width={90} height={90}></Image>
+                            </a>
+                        </div>
+                    </div>
+                </section>
+            </EmptyLayout>
+        </>
 
   );
 }
