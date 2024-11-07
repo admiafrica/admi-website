fe531f7c5c1f42ff62fec744960e8972c167fb05
John Wayodi <wayodijohn@gmail.com>
2024-11-07 18:48:15 +0300

    update script id

--- src/components/shared/GoogleAnalyticsTag.tsx
+++ src/components/shared/GoogleAnalyticsTag.tsx
@@ -1,31 +1,33 @@
 import Script from "next/script";
 
 type Props = {
-  googleTag: string;
+  anayticsId: string;
 };
 
 export default function GoogleAnalyticsTag(props: Props) {
+  const scriptId = `google-tag-manager-${props.anayticsId}`;
+
   return (
     <>
       <Script
-        id="google-tag-manager"
+        id={scriptId}
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id=' + '${props.googleTag}' + dl;
+            'https://www.googletagmanager.com/gtm.js?id=' + '${props.anayticsId}' + dl;
             f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','${props.googleTag}');
+            })(window,document,'script','dataLayer','${props.anayticsId}');
           `,
         }}
       />
       {/* Noscript Google Tag Manager */}
-      {props.googleTag && (
+      {props.anayticsId && (
         <noscript>
           <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${props.googleTag}`}
+            src={`https://www.googletagmanager.com/ns.html?id=${props.anayticsId}`}
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
