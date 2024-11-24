ed0300c3f1d5bef9add729097aff9932cfa7495d
John Wayodi <wayodijohn@gmail.com>
2024-11-24 14:47:42 +0300

    [admi] update lead form

--- src/pages/courses/index.tsx
+++ src/pages/courses/index.tsx
@@ -3,7 +3,7 @@ import { MainLayout } from "@/layouts/MainLayout";
 export default function CoursesRoute() {
   return (
     <MainLayout>
-      <p className="p-8">Courses Page</p>
+      <p className="p-8 hidden">Courses Page</p>
     </MainLayout>
   );
 }
