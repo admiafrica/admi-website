561895b3597626b9c0e05178df8a8a86f454a46a
John Wayodi <wayodijohn@gmail.com>
2025-02-03 07:49:59 +0300

    update assets

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,15 +1,12 @@
-import { IContentfulEntry } from "@/types";
+import { IContentfulAsset } from '@/types';
 
-export * from "./constants";
+export * from './constants';
 
 export const getCourseFormUrl = () => {
   return `https://airtable.com/app0kRJindIHzHTM2/pagmXFb9WKJbimfFa/form`;
 };
 
-export const getAssetDetails = (
-  assets: IContentfulEntry[],
-  assetId: string
-) => {
+export const getAssetDetails = (assets: IContentfulAsset[], assetId: string) => {
   const entry = assets.find((item) => item.sys.id === assetId);
   return entry;
 };
@@ -19,8 +16,8 @@ export function processVideoUrl(videoUrl: string): string {
   const youtubeRegex = /(?:https?:\/\/)?(?:www\.)?youtube\.com\/watch\?v=([a-zA-Z0-9_-]+)/;
   const match = videoUrl.match(youtubeRegex);
   if (match && match[1]) {
-      const videoId = match[1];
-      return `https://www.youtube.com/embed/${videoId}`;
+    const videoId = match[1];
+    return `https://www.youtube.com/embed/${videoId}`;
   }
   return videoUrl; // Return the original URL if it's not a YouTube link with `/watch`
 }
