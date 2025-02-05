422d0dee6fbda236d88155d118d55ad195b3960b
John Wayodi <wayodijohn@gmail.com>
2025-02-05 09:32:55 +0300

    add about page

---
+++ src/pages/v3/about.tsx
@@ -0,0 +1,242 @@
+import Image from 'next/image';
+import { Box, Text } from '@mantine/core';
+import { Carousel } from '@mantine/carousel';
+
+import { Paragraph, Title } from '@/components/ui';
+import { MainLayout } from '@/layouts/v3/MainLayout';
+
+import { PageSEO, Timeline } from '@/components/shared/v3';
+import { FacilityItemCard, SectorItemCard, CompanyValuesCard, UserProfileCard } from '@/components/cards';
+import { ADMI_ACADEMIC_TEAM, ADMI_DIRECTORS, ADMI_FACILITIES, ADMI_HISTORY, ADMI_VALUES } from '@/utils';
+
+import ImageAboutLanding from '@/assets/images/about-landing.svg';
+import IconBgImageYellow from '@/assets/icons/ellipse-yellow.svg';
+import IconBgImageRed from '@/assets/icons/ellipse-red.svg';
+
+import IconHome from '@/assets/icons/Home';
+import IconTripodCamera from '@/assets/icons/TripodCamera';
+import IconCamera from '@/assets/icons/Camera';
+import IconArrowTipRight from '@/assets/icons/ArrowTipRight';
+import IconSoundwave from '@/assets/icons/Soundwave';
+import IconTv from '@/assets/icons/Tv';
+import { useRouter } from 'next/router';
+
+export default function AboutPage() {
+  const router = useRouter();
+  const sectors = [
+    {
+      title: '2D and 3D animation',
+      icon: <IconHome color="#B9C601" />,
+    },
+    {
+      title: 'Video Production',
+      icon: <IconCamera color="#01C6A5" />,
+    },
+    {
+      title: 'Graphic Design',
+      icon: <IconTv color="#01C6A5" />,
+    },
+    {
+      title: 'Film & TV Production',
+      icon: <IconTripodCamera color="#F76335" />,
+    },
+    {
+      title: 'Sound Engineering',
+      icon: <IconSoundwave color="#B9C601" />,
+    },
+  ];
+
+  const handleViewCourses = () => {
+    router.push('/v3/courses');
+  };
+
+  return (
+    <MainLayout footerBgColor="#F5FFFD">
+      <PageSEO title="About Us" />
+      {/* HEADER */}
+      <div className="h-[24vh] w-full bg-[#002A23]">
+        {/* BACKGROUND IMAGES */}
+        <div className="absolute left-[63%] top-[10vh] z-0 h-fit w-full -translate-x-1/2 transform">
+          <div className="flex w-full justify-end pr-[10%]">
+            <Image src={IconBgImageYellow} alt={'background image'} />
+          </div>
+        </div>
+
+        <div className="absolute left-1/2 top-[5vh] z-0 h-fit w-full -translate-x-1/2 transform">
+          <div className="flex w-full">
+            <Image src={IconBgImageRed} alt={'background image'} />
+          </div>
+        </div>
+        <div className="relative z-10 mx-auto w-full max-w-screen-lg px-4 pt-24 2xl:px-0">
+          <div className="text-center font-nexa text-white">
+            <Text fw={900} size="46px">
+              Africa Digital Media <br></br> Institute <span className="text-admiShamrok">(ADMI)</span>
+            </Text>
+          </div>
+        </div>
+      </div>
+      {/* WHO WE ARE */}
+      <Box className="relative z-10 w-full bg-[#002A23]">
+        <Box className="relative h-[460px] w-full">
+          <Image src={ImageAboutLanding} alt={'background image'} fill objectFit="cover" />
+        </Box>
+        <Box className="mx-auto flex w-full max-w-screen-2xl">
+          <Box className="w-[50%] py-32 font-nexa text-white">
+            <Text size="18px" mb={32}>
+              Africa Digital Media Institute (ADMI) based in Nairobi, Kenya, is Eastern Africa’s premier creative media
+              and technology training institution.{' '}
+            </Text>
+
+            <Text size="18px" mb={32}>
+              It is an innovative career accelerator where creatives and techies receive training, mentorship, and a
+              platform to turn their passion into a profession.{' '}
+            </Text>
+
+            <Text fw={900} size="18px">
+              Our programs combine high-spec technical training in creative media and technology with intensive digital
+              and soft-skills coaching, along with a rigorous internship process, helping to achieve above-market
+              placement rates.
+            </Text>
+          </Box>
+          <Box className="w-[50%] py-16">
+            <Box className="flex w-full flex-wrap">
+              {sectors.map((sector: any) => (
+                <Box key={sector.title} className="mb-4 mr-4">
+                  <SectorItemCard
+                    sector={sector}
+                    textColor="white"
+                    bgColor="#092E28"
+                    withBorder
+                    defaultBorder={false}
+                  />
+                </Box>
+              ))}
+              <Box
+                className="mb-4 mr-4 flex w-[240px] cursor-pointer flex-col items-center justify-center rounded-lg bg-admiRed"
+                onClick={handleViewCourses}
+              >
+                <IconArrowTipRight width={48} height={48} color="white" />
+                <Paragraph className="text-white" fontFamily="font-nexa" fontWeight={900}>
+                  View all courses
+                </Paragraph>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+      {/* OUR HISTORY */}
+      <Box className="w-full py-8" bg={'#F5FFFD'}>
+        <Box className="mx-auto w-full max-w-screen-2xl">
+          <Box className="flex w-full flex-row">
+            <Box className="w-[30%]">
+              <Title label="Our History" color="black" />
+            </Box>
+            <Box className="w-[70%]">
+              <Timeline data={ADMI_HISTORY} />
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+      {/* OUR VALUES */}
+      <Box className="relative w-full pb-16" bg={'#F5FFFD'}>
+        <Box className="mx-auto w-full max-w-screen-2xl pb-10">
+          <Box className="flex w-full flex-row">
+            <Box className="w-[30%]">
+              <Title label="Our Values" color="black" />
+            </Box>
+            <Box className="w-[70%]">
+              <Paragraph size="20px" className="pt-4">
+                Our values at ADMI guide everything we do, from fostering creativity and innovation to ensuring
+                excellence and integrity in all our programs. We are committed to nurturing the next generation of
+                creative professionals with passion and purpose.
+              </Paragraph>
+            </Box>
+          </Box>
+        </Box>
+        {/* Floating Card */}
+        <div className="w-full px-1">
+          <div className="absolute left-1/2 top-[10vh] z-10 w-full max-w-screen-2xl -translate-x-1/2 transform px-4 sm:top-[10vh] 2xl:px-0">
+            <CompanyValuesCard values={ADMI_VALUES} />
+          </div>
+        </div>
+      </Box>
+      {/* ACADEMIC TEAM */}
+      <Box className="w-full pt-56" bg={'admiRed'}>
+        <Box className="mx-auto w-full max-w-screen-2xl">
+          <Box className="flex w-full flex-row">
+            <Box className="w-[30%]">
+              <Title label="Academic Team" color="white" />
+            </Box>
+          </Box>
+          <Box className="flex w-full flex-row flex-wrap py-8">
+            {ADMI_ACADEMIC_TEAM.map((member, index) => (
+              <div className="mr-8 w-fit" key={`academic-member-${index}`}>
+                <UserProfileCard user={member} />
+              </div>
+            ))}
+          </Box>
+        </Box>
+      </Box>
+      {/* BOARD OF DIRECTORS */}
+      <Box className="w-full py-8" bg={'#F5FFFD'}>
+        <Box className="mx-auto w-full max-w-screen-2xl">
+          <Box className="flex w-full flex-row">
+            <Box className="w-[30%]">
+              <Title label="Board of Directors" color="black" />
+            </Box>
+            <Box className="w-[70%]">
+              <Paragraph size="20px" className="pt-4">
+                ADMI’s campus is a vibrant, creatively designed workspace conveniently located right next to the GPO in
+                Nairobi’s Central Business District. The campus boasts extensive facilities including classrooms,
+                animation and graphics labs, TV and sound production studios, Mac and PC labs, and a film equipment
+                vault.
+              </Paragraph>
+            </Box>
+          </Box>
+          <Box className="flex w-full flex-row flex-wrap py-8">
+            {ADMI_DIRECTORS.map((member, index) => (
+              <div className="mr-8 w-fit" key={`academic-member-${index}`}>
+                <UserProfileCard user={member} />
+              </div>
+            ))}
+          </Box>
+        </Box>
+      </Box>
+      {/* FACILITIES */}
+      <Box className="1xl:px-0 w-full px-4 py-8" bg={'#F5FFFD'}>
+        <Box className="mx-auto w-full max-w-screen-2xl">
+          <Box className="flex w-full flex-row pb-12">
+            <Box className="w-[30%]">
+              <Title label="Facilities" color="black" />
+            </Box>
+            <Box className="w-[70%]">
+              <Paragraph size="20px" className="pt-4">
+                ADMI&apos;s campus is a vibrant, creatively designed workspace conveniently located right next to the
+                GPO in Nairobi&apos;s Central Business District. The campus boasts extensive facilities including
+                classrooms, animation and graphics labs, TV and sound production studios, Mac and PC labs, and a film
+                equipment vault.
+              </Paragraph>
+            </Box>
+          </Box>
+          <Box className="w-full">
+            <Carousel
+              slideSize={600}
+              height={360}
+              slideGap="md"
+              loop
+              align="start"
+              slidesToScroll={1}
+              controlsOffset={0}
+            >
+              {ADMI_FACILITIES.map((facility) => (
+                <Carousel.Slide key={facility.name}>
+                  <FacilityItemCard facility={facility} />
+                </Carousel.Slide>
+              ))}
+            </Carousel>
+          </Box>
+        </Box>
+      </Box>
+    </MainLayout>
+  );
+}
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,25 @@
+import StudiosImage from '@/assets/images/the-studios.svg';
+import SpacesImage from '@/assets/images/the-space.png';
+
+// User Profiles
+import ImageCaroline from '@/assets/images/caroline.png';
+import ImagePeter from '@/assets/images/peter.png';
+import ImageRaji from '@/assets/images/raji.png';
+import ImageCiku from '@/assets/images/ciku.png';
+
+import ImageLaila from '@/assets/images/laila.png';
+import ImageWilfred from '@/assets/images/wilfred.png';
+import ImageMugo from '@/assets/images/mugo.png';
+import ImageRoss from '@/assets/images/ross.png';
+import ImageChris from '@/assets/images/chris.png';
+
+// Icons
+import IconBrowser from '@/assets/icons/Browser';
+import IconDesktop from '@/assets/icons/Desktop';
+import IconGraphUp from '@/assets/icons/GraphUp';
+import IconMouse from '@/assets/icons/Mouse';
+import IconSpinner from '@/assets/icons/Spinner';
+
 export const ROUTES = {
   INDEX: '/',
   CAMPAIGNS: '/:country/:category/:campaign',
@@ -11,3 +33,197 @@ export const SOCIAL_LINKS = {
   YOUTUBE: 'https://www.youtube.com/@ADMIafrica/',
   X: 'https://twitter.com/ADMIafrica',
 };
+
+export const ADMI_FACILITIES = [
+  {
+    name: 'The Studios',
+    description:
+      'Our campus has industry-standard audio and TV studios. The TV studio has a tracking floor with lighting grid, while the music studio has a vocal booth that doubles up as rehearsal space that can accommodate a full band. The control room is equipped with professional software, including ProTools and Logic, as well as...',
+    image: StudiosImage,
+  },
+  {
+    name: 'The Space',
+    description:
+      'The ADMI campus at Caxton House has ample space to enable you to learn under the most conducive environment. The facilities include classrooms, animation and graphics labs, TV and sound production studios, Mac and PC labs, a film equipment vault and a fully stocked digital library.',
+    image: SpacesImage,
+  },
+  {
+    name: 'The Labs',
+    description:
+      'Our labs are fully equipped with both Mac and Pc computers and installed with industry-standard software, including Final Cut, Protools, Davinci Adobe Premier, Autodesk suite, Avid Composer and many more.',
+    image: '',
+  },
+  {
+    name: 'The Equipment',
+    description:
+      'We pride ourselves on being one of the few regional film schools that teach in HD, digital film formats. During your first year, you will hone your skills using small HD camcorders, before progressing to high-end HD formats and digital cinema cameras from popular manufacturers like Sony, Canon, Panasonic & Black...',
+    image: '',
+  },
+];
+
+export const ADMI_ACADEMIC_TEAM = [
+  {
+    name: 'Carolyne Sila',
+    title: 'Head of School',
+    image: ImageCaroline,
+  },
+  {
+    name: 'Peter Mwaniki',
+    title: 'Academics',
+    image: ImagePeter,
+  },
+  {
+    name: 'Raji Ilangovan',
+    title: 'Student Programs',
+    image: ImageRaji,
+  },
+  {
+    name: 'Ciku Munuku',
+    title: 'Faculty Affairs',
+    image: ImageCiku,
+  },
+];
+
+export const ADMI_DIRECTORS = [
+  {
+    name: 'Laila Macharia',
+    title: 'Co-Founder & Chair',
+    linkedin: '',
+    image: ImageLaila,
+  },
+  {
+    name: 'Wilfred Kiumi',
+    title: 'Co-Founder',
+    linkedin: '',
+    image: ImageWilfred,
+  },
+  {
+    name: 'Mugo Kibati',
+    title: 'Director',
+    linkedin: '',
+    image: ImageMugo,
+  },
+  {
+    name: 'Ross Franks',
+    title: 'Director',
+    linkedin: '',
+    image: ImageRoss,
+  },
+  {
+    name: 'Chris Foot',
+    title: 'Director',
+    linkedin: '',
+    image: ImageChris,
+  },
+];
+
+export const ADMI_HISTORY = [
+  {
+    year: 2011,
+    achievements: [
+      'Wiflred Kiumi establishes the Jamhuri Film and Television Academy(JFTA)',
+      'JFTA opens in 2012 with 5 students',
+    ],
+  },
+  {
+    year: 2012,
+    achievements: ['Film & TV classes begin', 'JFTA initiates first industry partnerships'],
+  },
+  {
+    year: 2014,
+    achievements: ['First ADMI campus established in nairoi through a partnership with JFTA'],
+  },
+  {
+    year: 2015,
+    achievements: ['First ADMI campus established in nairoi through a partnership with JFTA'],
+  },
+  {
+    year: 2016,
+    achievements: ['First ADMI campus established in nairoi through a partnership with JFTA'],
+  },
+  {
+    year: 2017,
+    achievements: ['First ADMI campus established in nairoi through a partnership with JFTA'],
+  },
+  {
+    year: 2018,
+    achievements: ['First ADMI campus established in nairoi through a partnership with JFTA'],
+  },
+  {
+    year: 2019,
+    achievements: ['First ADMI campus established in nairoi through a partnership with JFTA'],
+  },
+  {
+    year: 2020,
+    achievements: ['First ADMI campus established in nairoi through a partnership with JFTA'],
+  },
+  {
+    year: 2021,
+    achievements: ['First ADMI campus established in nairoi through a partnership with JFTA'],
+  },
+  {
+    year: 2022,
+    achievements: ['First ADMI campus established in nairoi through a partnership with JFTA'],
+  },
+  {
+    year: 2023,
+    achievements: ['First ADMI campus established in nairoi through a partnership with JFTA'],
+  },
+  {
+    year: 2024,
+    achievements: ['First ADMI campus established in nairoi through a partnership with JFTA'],
+  },
+];
+
+export const ADMI_VALUES = [
+  {
+    id: 'global',
+    name: 'Global',
+    icon: IconBrowser,
+    iconColor: '#F76335',
+    description: [
+      `By joining the ADMI community, you enjoy an exceptional international education benchmarking operations and pedagogy against global standards.`,
+      `To ensure that your skills are globally marketable, we develop our curriculum in partnership with global industry partners and faculty recruited from leading international academic institutions. And best of all, you will learn alongside a diverse community that has hosted staff, faculty and students from over 27 different countries.`,
+    ],
+  },
+  {
+    id: 'practical',
+    name: 'Practical',
+    icon: IconMouse,
+    iconColor: '#03C6A4',
+    description: [
+      `At the heart of our education is our innovative Learn-and-Work model. We ensure that our faculty are inductry practitioners, not academics, so that you as a student keep up to date on the latest practices and networks.`,
+      `You also get access to high-end equipment, software and facilities just as you would in the workplace. And we work closely with employer partners to ensure that you get early industry exposure to start building your professional portfolio from day 1.`,
+    ],
+  },
+  {
+    id: 'digital',
+    name: 'Digital',
+    icon: IconDesktop,
+    iconColor: '#F60934',
+    description: [
+      `ADMI aspires to be a paperless campus with the latest e-learning tools and state of the art computer labs at a 1:1 computer to student ratio.`,
+      `As a student you can access financing from our patners to get industry -standard equipment through our Creative Toolbox Program.Our core faculty are all certified in online instruction, enabling them to deliver distance learning to the highest standards`,
+    ],
+  },
+  {
+    id: 'value-driven',
+    name: 'Value Driven',
+    icon: IconGraphUp,
+    iconColor: '#871F00',
+    description: [
+      `ADMI is committed to a progressive and positive schol atmosphere where our students and staff can thrive.`,
+      `We striuctly enforce ethical behavior on campus and apply global professional standards in all our interactions. To ensure that studens receive personalized attention, we offer string psycho-social support, including academic suport as well as progressive guidance counselling.`,
+    ],
+  },
+  {
+    id: 'transformational',
+    name: 'Transformational',
+    icon: IconSpinner,
+    iconColor: '#B9C503',
+    description: [
+      `Want to see your child change before your eyes, from disengaged student to passionate professional?`,
+      `ADMI is a selective and intimate school with a low teacher -to-student ratio allowing us to closely monitor individual student progress. This way, we strive to ensure that ADMI is a "defining experience" for each student, teacher and employee.`,
+    ],
+  },
+];
