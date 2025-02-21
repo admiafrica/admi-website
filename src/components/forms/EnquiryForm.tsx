d2247b4cfcc13f2c18a934e9383738913364522b
John Wayodi <wayodijohn@gmail.com>
2025-02-21 17:41:23 +0300

    update enquiry form

--- src/components/forms/EnquiryForm.tsx
+++ src/components/forms/EnquiryForm.tsx
@@ -1,15 +1,18 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useCallback, useEffect, useState } from 'react';
-import { Alert, Group, Select, Text, TextInput } from '@mantine/core';
+import { Alert, Box, Group, Select, Text, TextInput } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import { Button, Paragraph, Title } from '../ui';
+import PhoneInput from 'react-phone-input-2';
+import 'react-phone-input-2/lib/style.css';
 
 import { IconAsterisk } from '@tabler/icons-react';
 
 export default function EnquiryForm() {
   const router = useRouter();
   const [courses, setCourses] = useState<any[]>([]);
+  const [countryCode, setCountryCode] = useState('254'); // State for the phone number
   const [alert, setAlert] = useState<{ type: 'success' | 'error'; message: string } | null>(null);
 
   const form = useForm({
@@ -40,13 +43,20 @@ export default function EnquiryForm() {
   const handleSubmit = async (values: any) => {
     setAlert(null); // Clear previous alerts
 
+    // always remove leading zero from phone incase included
+    const formattedPhone = values.phone.replace(/^0+/, '');
+    const data = {
+      ...values,
+      phone: `${countryCode}${formattedPhone}`,
+    };
+
     try {
       const response = await fetch('/api/v3/push-lead', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(values),
+        body: JSON.stringify({ ...data }),
       });
 
       if (!response.ok) {
@@ -56,7 +66,6 @@ export default function EnquiryForm() {
       }
       router.push('/v3/enquiry-thank-you');
     } catch (error) {
-      // console.error('Error submitting enquiry:', error);
       setAlert({ type: 'error', message: 'An error occurred. Please try again later.' });
     }
   };
@@ -138,19 +147,42 @@ export default function EnquiryForm() {
           key={form.key('lastName')}
           {...form.getInputProps('lastName')}
         />
-        <TextInput
-          my={16}
-          className="border-1 rounded-lg border-solid border-gray-200 py-2"
-          label={
-            <div className="flex pl-2">
-              <Title label="Phone Number" color="black" size="1.4em" />
-              <IconAsterisk size={8} className="mt-1.5 text-admiRed" />
-            </div>
-          }
-          placeholder="Enter phone i.e +254723..."
-          key={form.key('phone')}
-          {...form.getInputProps('phone')}
-        />
+        <Box my={16} className="border-1 rounded-lg border-solid border-gray-200 py-2">
+          <div className="flex pl-2">
+            <Title label="Phone Number" color="black" size="1.4em" />
+            <IconAsterisk size={8} className="mt-1.5 text-admiRed" />
+          </div>
+          <Box className="flex">
+            <PhoneInput
+              country={'ke'}
+              value={countryCode}
+              onChange={(value) => {
+                setCountryCode(value);
+              }}
+              containerStyle={{
+                border: 'none',
+                width: 100,
+              }}
+              inputStyle={{
+                border: 'none',
+              }}
+              buttonStyle={{
+                border: 'none', // Remove dropdown button border
+                marginLeft: '8px',
+              }}
+              dropdownStyle={{
+                border: 'none', // Remove dropdown border
+                boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', // Optional: Add a subtle shadow
+              }}
+            />
+            <TextInput
+              className="grow"
+              placeholder="Enter phone i.e 0700000000"
+              key={form.key('phone')}
+              {...form.getInputProps('phone')}
+            />
+          </Box>
+        </Box>
         <Group justify="flex-end" mt="2em" className="w-full">
           <Button size="lg" backgroundColor="admiRed" label="Submit" type="submit" />
         </Group>
@@ -167,3 +199,11 @@ export default function EnquiryForm() {
     </div>
   );
 }
+
+const numberInputStyle: React.CSSProperties = {
+  border: 'none',
+};
+
+const numberDropdownStyle: React.CSSProperties = {
+  border: 'none',
+};
