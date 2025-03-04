24f8bc0f373192b90d8aa95faea5656530eed777
John Wayodi <wayodijohn@gmail.com>
2025-03-04 17:10:10 +0300

    use social share component

--- src/pages/sanara.tsx
+++ src/pages/sanara.tsx
@@ -2,63 +2,28 @@ import Image from 'next/image';
 import { Anchor, Box, Card, Divider } from '@mantine/core';
 
 import { MainLayout } from '@/layouts/v3/MainLayout';
-import { PageSEO } from '@/components/shared/v3';
+import { PageSEO, SocialShare } from '@/components/shared/v3';
 import { Button, Paragraph } from '@/components/ui';
 import { useIsMobile } from '@/hooks/useIsMobile';
 
-import IconFacebook from '@/assets/icons/facebook-social.svg';
-import IconWhatsapp from '@/assets/icons/whatsapp-social.svg';
-import IconCopyContent from '@/assets/icons/copy-content.svg';
-import IconShare from '@/assets/icons/share.svg';
-import IconLinkedIn from '@/assets/icons/linkedin-social.svg';
 import ImageSanaraLanding from '@/assets/images/sanara-landing.png';
 
 export default function NewsArticlePage() {
+  const article = {
+    title: 'ADMI-Sanara Animation Program',
+    summary: `Sanara is inspired by the Swahili words sanaa (arts) and biashara (business). This
+              exciting inclusive program designed for young women and men across Kenya aims to enable access to
+              dignified and fulfilling work in the creative and cultural industries.`,
+  };
   const isMobile = useIsMobile();
 
   return (
     <MainLayout footerBgColor="white">
-      <PageSEO title="Sanara " />
+      <PageSEO title="Sanara Animation Program" description="" image={ImageSanaraLanding} />
       <Box className="w-full">
         <Box className="mx-auto flex w-full max-w-screen-xl flex-col-reverse px-4 py-4 sm:flex-row sm:py-16 xl:px-0">
           <Box className="sm:w-[200px]">
-            <Box className="sticky top-[100px] flex flex-col sm:w-[200px]">
-              <Box className="flex pb-6">
-                <Image src={IconShare} alt="share" width={32} height={32} />
-                <Paragraph className="my-auto" fontFamily="font-nexa">
-                  Share on:
-                </Paragraph>
-              </Box>
-              <Card className="flex w-full flex-col" shadow="lg">
-                <Box className="flex py-4">
-                  <Image src={IconFacebook} alt="facebook" width={24} height={24} />
-                  <Paragraph fontFamily="font-nexa" size="16px" className="my-auto ml-2">
-                    Facebook
-                  </Paragraph>
-                </Box>
-                <Divider />
-                <Box className="flex py-4">
-                  <Image src={IconLinkedIn} alt="facebook" width={24} height={24} />
-                  <Paragraph fontFamily="font-nexa" size="16px" className="my-auto ml-2">
-                    LinkedIn
-                  </Paragraph>
-                </Box>
-                <Divider />
-                <Box className="flex py-4">
-                  <Image src={IconWhatsapp} alt="facebook" width={24} height={24} />
-                  <Paragraph fontFamily="font-nexa" size="16px" className="my-auto ml-2">
-                    Whatsapp
-                  </Paragraph>
-                </Box>
-                <Divider />
-                <Box className="flex py-4">
-                  <Image src={IconCopyContent} alt="facebook" width={24} height={24} />
-                  <Paragraph fontFamily="font-nexa" size="16px" className="my-auto ml-2">
-                    Copy Link
-                  </Paragraph>
-                </Box>
-              </Card>
-            </Box>
+            <SocialShare item={article} />
           </Box>
           <Card className="mb-6 min-h-[80vh] w-full sm:ml-8" withBorder>
             <Box className="relative" h={isMobile ? '200px' : '500px'}>
