83b1c1361413c33275eca8b4dd2ba73c13d9704f
Wilfred K <83074782+wilfredkiumi@users.noreply.github.com>
2025-06-06 23:01:40 +0100

    fix: resolve Next.js Script warning by migrating GTM to next/script
    - Create GoogleTagManager component using next/script with afterInteractive strategy
    - Move GTM script from _document.tsx to _app.tsx for proper Script component usage
    - Keep noscript fallback in _document.tsx as required by GTM implementation
    - Remove inline script warning that was appearing during build
    - Maintain GTM functionality while following Next.js best practices
    - Build now completes without any warnings related to Google Analytics/GTM

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,22 +5,7 @@ export default function Document() {
 
   return (
     <Html lang="en">
-      <Head>
-        {/* Google Tag Manager */}
-        {gtmId && (
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','${gtmId}');
-              `
-            }}
-          />
-        )}
-      </Head>
+      <Head>{/* GTM script moved to _app.tsx using next/script */}</Head>
       <body className="antialiased">
         {/* Google Tag Manager (noscript) */}
         {gtmId && (
