455a45f0c37e6bae9fd67dfd88c2ab10a831b83d
John-Admi <john@admi.africa>
2025-06-12 12:36:45 +0300

    Update NavBar.tsx
    nav bar update

--- src/components/shared/v3/NavBar.tsx
+++ src/components/shared/v3/NavBar.tsx
@@ -48,7 +48,7 @@ export default function NavBar({ mode, isMinimal = false }: Props) {
             </Text>
           </Menu.Target>
         </Menu>
-  
+        
         <Menu trigger="hover" openDelay={100} closeDelay={400}>
           <Menu.Target>
             <Text style={menuItemStyle} className="cursor-pointer" onClick={() => navigateToPage('ai-academy')}>
