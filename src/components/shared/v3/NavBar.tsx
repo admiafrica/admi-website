11bc359bc741760ec843d9252c64fc4ba5548862
John-Admi <john@admi.africa>
2025-06-12 11:02:35 +0300

    Update NavBar.tsx
    navigation menu update

--- src/components/shared/v3/NavBar.tsx
+++ src/components/shared/v3/NavBar.tsx
@@ -51,20 +51,12 @@ export default function NavBar({ mode, isMinimal = false }: Props) {
 
        <Menu trigger="hover" openDelay={100} closeDelay={400}>
           <Menu.Target>
-            <Text style={menuItemStyle} className="cursor-pointer" onClick={() => window.open('https://ai.admi.africa', '_blank')}>
+            <Text style={menuItemStyle} className="cursor-pointer" onClick={() => navigateToPage('student-support')}>
               Ai Academy
             </Text>
           </Menu.Target>
         </Menu>
-
-        <Menu trigger="hover" openDelay={100} closeDelay={400}>
-          <Menu.Target>
-            <Text style={menuItemStyle} className="cursor-pointer" onClick={() => window.open('https://www.externalwebsite.com', '_blank')}>
-              Ai Academy
-            </Text>
-          </Menu.Target>
-        </Menu>
-        
+     
         <Menu trigger="hover" openDelay={100} closeDelay={400}>
           <Menu.Target>
             <Text style={menuItemStyle} className="cursor-pointer" onClick={() => navigateToPage('student-support')}>
