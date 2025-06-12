a82bcd283959e4655b5b223197e15f329d4d35b8
John-Admi <john@admi.africa>
2025-06-12 06:11:39 +0300

    Update NavBar.tsx
    testing navbar updating

--- src/components/shared/v3/NavBar.tsx
+++ src/components/shared/v3/NavBar.tsx
@@ -60,7 +60,7 @@ export default function NavBar({ mode, isMinimal = false }: Props) {
         <Menu trigger="hover" openDelay={100} closeDelay={400}>
           <Menu.Target>
             <Text style={menuItemStyle} className="cursor-pointer" onClick={() => navigateToPage('resources')}>
-              Resources
+              Resource
             </Text>
           </Menu.Target>
         </Menu>
