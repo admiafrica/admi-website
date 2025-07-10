ddc3bbc0b8394fd9a19663080254412909ed2d85
Wilfred K <83074782+wilfredkiumi@users.noreply.github.com>
2025-07-10 16:10:14 +0100

    fix(seo): update robots.txt to allow crawling of courses

--- src/pages/robots.txt.tsx
+++ src/pages/robots.txt.tsx
@@ -7,25 +7,20 @@ const RobotsTxt = () => {
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
   const robotsTxt = `User-agent: *
 Allow: /
-
-# SEO-optimized pages
 Allow: /frequently-asked-questions
 Allow: /careers/*
 Allow: /courses/*
 
-# Sitemaps
 Sitemap: https://admi.africa/sitemap.xml
 Sitemap: https://admi.africa/video-sitemap.xml
 Sitemap: https://admi.africa/news-sitemap.xml
 
-# Crawl delays for different bots
 User-agent: Googlebot
 Crawl-delay: 1
 
 User-agent: Bingbot
 Crawl-delay: 2
 
-# Block spam bots
 User-agent: AhrefsBot
 Disallow: /
 
@@ -35,13 +30,10 @@ Disallow: /
 User-agent: DotBot
 Disallow: /
 
-# Block admin and private areas
 Disallow: /admin/
 Disallow: /api/
 Disallow: /student-portal/login
-
-# Cache directives
-Cache-Control: public, max-age=86400`
+`
 
   res.setHeader('Content-Type', 'text/plain')
   res.write(robotsTxt)
