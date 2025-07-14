c61f90c55275fd81714356777db03f6cde249657
Wilfred K <83074782+wilfredkiumi@users.noreply.github.com>
2025-07-15 00:51:59 +0100

    fix(robots): allow all crawlers

--- src/pages/robots.txt.tsx
+++ src/pages/robots.txt.tsx
@@ -7,35 +7,13 @@ const RobotsTxt = () => {
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
   const robotsTxt = `User-agent: *
 Allow: /
-Allow: /frequently-asked-questions
-Allow: /careers/*
-Allow: /courses/
-Allow: /courses/*
-Allow: /news-events/
-Allow: /watch/
+Disallow: /admin/
+Disallow: /api/
+Disallow: /student-portal/login
 
 Sitemap: https://admi.africa/sitemap.xml
 Sitemap: https://admi.africa/video-sitemap.xml
 Sitemap: https://admi.africa/news-sitemap.xml
-
-User-agent: Googlebot
-Crawl-delay: 1
-
-User-agent: Bingbot
-Crawl-delay: 2
-
-User-agent: AhrefsBot
-Disallow: /
-
-User-agent: MJ12bot
-Disallow: /
-
-User-agent: DotBot
-Disallow: /
-
-Disallow: /admin/
-Disallow: /api/
-Disallow: /student-portal/login
 `
 
   res.setHeader('Content-Type', 'text/plain')
